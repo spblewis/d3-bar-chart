9ab6ff112c715927c371829df3a94db4478470f2
spblewis <spblewis@gmail.com>
2021-06-02 17:17:37 -0500

    Refactored so that fetching data is handled by D3 methods, rather than javascript

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-         .then(response => response.json())
-         .then(data => {
-             document.getElementById('dummy').innerHTML = JSON.stringify(data);
-         })
-})
+const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+const { json } = d3;
+
+
+json(source)
+  .then(data => {
+      document.getElementById('dummy').innerHTML = JSON.stringify(data)
+    });
