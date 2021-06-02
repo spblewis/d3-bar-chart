dc8e9c66f285f5302479d75099d519b061d53069
spblewis <spblewis@gmail.com>
2021-06-02 17:21:41 -0500

    Defined svg area

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-const { json } = d3;
+const { json, select } = d3;
 
+const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+
+const height = 500;
+const width = 800;
+
+const svg = select('svg')
+    .attr('width', width)
+    .attr('height', height);
 
 json(source)
   .then(data => {
