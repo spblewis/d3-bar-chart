9f8d1b965b53ac83784e106fd74e9e238dccd4a1
spblewis <spblewis@gmail.com>
2021-06-03 11:52:01 -0500

    x axis in place

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const {
+  json,
+  select,
+  scaleLinear,
+  max,
+  axisBottom,
 // eslint-disable-next-line no-undef
-const { json, select } = d3;
+} = d3;
 
 const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
 const height = 500;
 const width = 800;
+const padding = 30;
 
 const svg = select('svg')
   .attr('width', width)
@@ -12,9 +19,17 @@ const svg = select('svg')
 
 json(source)
   .then((data) => {
+    // scale for x axis
+    const xScale = scaleLinear()
+      .domain([0, max(data.data, (d) => d[0])])
+      .range([padding, width - padding]);
+
+    const xAxis = axisBottom(xScale);
     // x axis
     svg.append('g')
-      .attr('id', 'x-axis');
+      .attr('id', 'x-axis')
+      .attr('transform', `translate(0, ${height - padding})`)
+      .call(xAxis);
 
     // y axis
     svg.append('g')
